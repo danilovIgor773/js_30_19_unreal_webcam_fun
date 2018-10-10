568e06c502f4480350295762f2e713085b26de99
Danilov Igor <danilovigor773@gmail.com>
2018-10-10 16:39:57 +0500

    Lesson not finished. Video tag wwrong dimensions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './index.scss';
 
-console.log("Hello World");
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+//---getVideo() -function gets the video from the webcam and puts it on the stream
+function getVideo(){
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`Error detected`, err);
+    });
+}
+
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(video);
+
+
+  console.log(width, height);
+  canvas.width = width;
+  canvas.height = height;
+
+
+  console.log(width, height);
+  setInterval(function(){
+    ctx.drawImage(video, 0, 0, width, height);
+  },16);
+}
+
+//paintToCanvas();
+getVideo();
+paintToCanvas();
+
+
+
+//console.log("Hi there!");
